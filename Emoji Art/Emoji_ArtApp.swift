d484509e8b3eef2ef97327acb32e69a3ea8f1369
Gustavo <gustavo_fior@outlook.com>
2024-06-27 20:56:01 -0300

    Initial Commit

---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Gustavo Fior on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
